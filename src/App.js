d64d7b579190c546f9676ab2bccfe09a561eb721
khanh.dinhtrong <19127437@student.hcmus.edu.vn>
2022-11-24 11:15:53 +0700

    done UI edit profile and UI create new group

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import "./App.css";
 import { Button } from "react-bootstrap";
 import { BrowserRouter as Router, Link, Route, Routes } from "react-router-dom";
-import ListGroup from "./pages/listGroup";
+import ListGroup from "./pages/ListGroup";
+import CreateGroup from "./pages/CreateGroup";
+import EditProfile from "./pages/profile/EditProfile";
 function App() {
   return (
     <Router>
       <Routes>
         <Route exact path="/" element={<ListGroup />} />
+        <Route exact path="/create-group" element={<CreateGroup />} />
+        <Route exact path="/profile/:id" element={<EditProfile />} />
       </Routes>
     </Router>
   );
