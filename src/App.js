4e13737d5b32739c6d210a13384b80328001310e
lthieu288 <letrunghieu2882001@gmail.com>
2022-11-26 18:15:15 +0700

    protected routes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
 import "./App.css";
-import { Routes, Route } from "react-router-dom";
+import { Navigate, Routes, Route } from "react-router-dom";
 import Register from "./Pages/Register";
 import Login from "./Pages/Login";
+import GroupDetail from "./Pages/GroupDetail";
+import { useAuthState } from "./Context";
 
 function App() {
+  const userDetails = useAuthState();
   return (
     <>
       <Routes>
         <Route index element={<Login />} />
-        <Route path="login" element={<Login />} />
-        <Route path="register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route
+          path="/groups"
+          element={
+            !Boolean(userDetails.token) ? (
+              <Navigate to={"/"} replace />
+            ) : (
+              <GroupDetail />
+            )
+          }
+        />
         <Route path="*" element={<p>There's nothing here: 404!</p>} />
       </Routes>
     </>
---
+++ src/Context/reducer.js
@@ -0,0 +1,45 @@
+let user = localStorage.getItem("currentUser")
+  ? JSON.parse(localStorage.getItem("currentUser")).user
+  : "";
+
+let token = localStorage.getItem("currentUser")
+  ? JSON.parse(localStorage.getItem("currentUser")).auth_token
+  : "";
+
+export const initialState = {
+  userDetails: "" || user,
+  token: "" || token,
+  loading: false,
+  errorMessage: null,
+};
+
+export const AuthReducer = (initialState, action) => {
+  switch (action.type) {
+    case "REQUEST_LOGIN":
+      return {
+        ...initialState,
+        loading: true,
+      };
+    case "LOGIN_SUCCESS":
+      return {
+        ...initialState,
+        user: action.payload.user,
+        token: action.payload.token,
+        loading: false,
+      };
+    case "LOGOUT":
+      return {
+        ...initialState,
+        user: "",
+        token: "",
+      };
+    case "LOGIN_ERROR":
+      return {
+        ...initialState,
+        loading: false,
+        errorMessage: action.error,
+      };
+    default:
+      throw new Error(`Unhandle action type: ${action.type}`);
+  }
+};
