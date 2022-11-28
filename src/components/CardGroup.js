4453545a684386d53fba1c055aad3fb6bd059912
Trong Khanh <64051785+19127437@users.noreply.github.com>
2022-11-28 22:19:00 +0700

    update API + UI version 1.0 (#5)

---
+++ src/components/CardGroup.js
@@ -0,0 +1,36 @@
+import React from "react";
+import '../styles/CreateGroup.module.css'
+import {Link} from "react-router-dom";
+import Card from "react-bootstrap/Card";
+
+const CardGroup = (props) => {
+    return (
+        <>
+            <Card
+                className="item-group"
+                style={{padding: "0", borderRadius: "15px", margin: 0}}
+            >
+                <Link>
+                    <Card.Img
+                        variant="top"
+                        src="https://i1-thethao.vnecdn.net/2022/11/20/ronaldo-messi-jpeg-2695-1668938162.jpg?w=0&h=0&q=100&dpr=2&fit=crop&s=Lv4T90hu_MaLuArTfZH4sA"
+                        style={{
+                            width: "100%",
+                            height: "14rem",
+                            cursor: "pointer",
+                            borderTopLeftRadius: "15px",
+                            borderTopRightRadius: "15px",
+                        }}
+                    />
+                </Link>
+                <Card.Body>
+                    <div className="name-group" style={{cursor: "pointer"}}>
+                        <a style={{fontWeight: "bolder"}}>{props.nameGroup}</a>
+                    </div>
+                </Card.Body>
+            </Card>
+        </>
+    );
+};
+
+export default CardGroup;
--- src/services/auth.js
+++ src/services/auth.js
@@ -1,13 +1,55 @@
 import request from "../utils/request";
 
-var optionAxios = {
-  headers: "Access-Control-Allow-Origin: *",
+const optionAxios = {
+    headers: "Access-Control-Allow-Origin: *",
 };
+
 export const register = async (userInfo) => {
-  const res = await request
-    .post("register", userInfo, optionAxios)
-    .then((res) => {
-      console.log("res: ", res);
-    });
-  return res.data;
+    const res = await request
+        .post("register", userInfo, optionAxios)
+        .then((res) => {
+            console.log("res: ", res);
+        });
+    return res.data;
 };
+
+export async function createGroup(body,token) {
+    console.log(token + "--" + body)
+    const response = await fetch('http://localhost:7777/accounts/create-group', {
+        method: 'POST',
+        headers: {'Content-type': 'application/json; charset=UTF-8', 'Authorization': token},
+        body: JSON.stringify(body),
+    });
+    if (response.status === 201) return response;
+    else return response.json();
+}
+
+export async function getProfile(token) {
+    const response = await fetch('http://localhost:7777/accounts/profile', {
+        headers: {
+            Authorization: token,
+            'Content-type': 'application/json; charset=UTF-8'
+        },
+    });
+    return response.json();
+}
+
+export async function editProfile(token, userEdit) {
+    const response = await fetch('http://localhost:7777/accounts/edit', {
+        method: 'POST',
+        headers: {'Content-type': 'application/json; charset=UTF-8', 'Authorization': token},
+        body: JSON.stringify(userEdit),
+    });
+    if (response.status === 204) return response;
+    else return response.json();
+}
+
+export async function getGroups(token) {
+    const response = await fetch('http://localhost:7777/group/get-all', {
+        headers: {
+            Authorization: token,
+            'Content-type': 'application/json; charset=UTF-8'
+        },
+    });
+    return response.json();
+}
