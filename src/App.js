e55673d22e73b7c06c5833319bbee369c0a21f05
lthieu288 <letrunghieu2882001@gmail.com>
2022-12-18 10:45:58 +0700

    change logic presentation

--- src/App.js
+++ src/App.js
@@ -1,9 +1,5 @@
 import "./App.css";
-import {
-    BrowserRouter as Router,
-    Route,
-    Routes,
-} from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import HomePage from "./Pages/Homepage";
 import CreateGroup from "./Pages/CreateGroup";
 import EditProfile from "./Pages/EditProfile";
@@ -17,33 +13,32 @@ import ViewForTheHost from "./Pages/Viewforthehost";
 import ChoiceQuestion from "./Pages/ChoiceQuestion";
 import Result from "./Pages/Result";
 function App() {
-return (
-        <>
-          <Router>
-            <Routes>
-              <Route path="/login" element={<Login />} />
-              <Route path="/register" element={<Register />} />
-              <Route index element={<HomePage />} />
-              <Route exact path="/" element={<HomePage />} />
-              <Route path="/group/:id" element={<GroupDetail />} />
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route index element={<HomePage />} />
+          <Route exact path="/" element={<HomePage />} />
+          <Route path="/group/:id" element={<GroupDetail />} />
 
-              <Route path="/presentation" element={<Presentation />} />
-              <Route path="/presentation/:id" element={<Slide />} />
-              <Route path="/view-host" element={<ViewForTheHost />} />
-              <Route path="/result/:id" element={<Result />} />
-              <Route path="/view-host/:id" element={<ChoiceQuestion />} />
-              <Route path="/profile" element={<EditProfile />} />
-              <Route exact path="/create-group" element={<CreateGroup />} />
-              <Route path="/join-group/:id" element={<JoinGroup />} />
-              <Route path="*" element={<p>There's nothing here: 404!</p>} />
-            </Routes>
-          </Router>
-        </>
-    );
+          <Route path="/presentation" element={<Presentation />} />
+          <Route path="/presentation/:id" element={<Slide />} />
+          <Route path="/view-host" element={<ViewForTheHost />} />
+          <Route path="/result/:id" element={<Result />} />
+          <Route path="/presentation/public/:id" element={<ChoiceQuestion />} />
+          <Route path="/profile" element={<EditProfile />} />
+          <Route exact path="/create-group" element={<CreateGroup />} />
+          <Route path="/join-group/:id" element={<JoinGroup />} />
+          <Route path="*" element={<p>There's nothing here: 404!</p>} />
+        </Routes>
+      </Router>
+    </>
+  );
 }
 
 export default App;
-
 
 // import {useEffect, useState} from "react";
 // function App() {
