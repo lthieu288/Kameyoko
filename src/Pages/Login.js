15bb95d51a7a7fcddb4db46001913bceb2396389
Dat Truong <dattruong@Dats-MacBook-Pro.local>
2022-12-23 23:44:14 +0700

    add second google auth

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -7,8 +7,8 @@ import Swal from "sweetalert2";
 import { GoogleLogin } from "react-google-login";
 import { loginUser, useAuthDispatch, useAuthState } from "../Context";
 import { gapi } from "gapi-script";
-const clientId =
-  "768128998994-6ltvdfgdgotov36pbbmqmv4apvjfsor5.apps.googleusercontent.com";
+// const clientId = "768128998994-6ltvdfgdgotov36pbbmqmv4apvjfsor5.apps.googleusercontent.com";
+const clientId = "12369507363-douqeq9o9pqaf40i4ij4rk1fql17t9nt.apps.googleusercontent.com";
 function Login() {
   const dispatch = useAuthDispatch();
   const [email, setEmail] = useState("");
@@ -114,6 +114,15 @@ function Login() {
                         onFailure={onFailure}
                         cookiePolicy={"single_host_origin"}
                       ></GoogleLogin>
+                    </div>
+                    <div className="form-outline mb-4 d-flex justify-content-center">
+                      <a href="http://localhost:7777/api/v1/oauth/google/login" style={{textTransform: 'none'}}>
+                        <div className="left">
+                          <img width="30px" alt="Google &quot;G&quot; Logo"
+                               src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/512px-Google_%22G%22_Logo.svg.png"/>
+                        </div>
+                        Login with Google
+                      </a>
                     </div>
                     <div className="d-flex justify-content-center">
                       <p className="mb-0 text-black center text-muted">
